db9e62936fb647557224e5d838e41e41f83ed48c
Manoj Kumar AVM <quic_mkavm@quicinc.com>
2023-10-19 12:14:02 -0700

    config: Add display-core-product.mk
    Change-Id: I35b7f62e0898e226aab27a3758da4a1189247165
    CRs-Fixed: 3651916

---
+++ config/display-core-product.mk
@@ -0,0 +1,8 @@
+PRODUCT_COPY_FILES += vendor/qcom/opensource/display-core/snapalloc/resources/camera_alignments.json:$(TARGET_COPY_OUT_VENDOR)/etc/display/camera_alignments.json
+PRODUCT_COPY_FILES += vendor/qcom/opensource/display-core/snapalloc/resources/cpu_alignments.json:$(TARGET_COPY_OUT_VENDOR)/etc/display/cpu_alignments.json
+PRODUCT_COPY_FILES += vendor/qcom/opensource/display-core/snapalloc/resources/default_alignments.json:$(TARGET_COPY_OUT_VENDOR)/etc/display/default_alignments.json
+PRODUCT_COPY_FILES += vendor/qcom/opensource/display-core/snapalloc/resources/display_alignments.json:$(TARGET_COPY_OUT_VENDOR)/etc/display/display_alignments.json
+PRODUCT_COPY_FILES += vendor/qcom/opensource/display-core/snapalloc/resources/formats.json:$(TARGET_COPY_OUT_VENDOR)/etc/display/formats.json
+PRODUCT_COPY_FILES += vendor/qcom/opensource/display-core/snapalloc/resources/graphics_alignments.json:$(TARGET_COPY_OUT_VENDOR)/etc/display/graphics_alignments.json
+PRODUCT_COPY_FILES += vendor/qcom/opensource/display-core/snapalloc/resources/ubwc_alignments.json:$(TARGET_COPY_OUT_VENDOR)/etc/display/ubwc_alignments.json
+PRODUCT_COPY_FILES += vendor/qcom/opensource/display-core/snapalloc/resources/video_alignments.json:$(TARGET_COPY_OUT_VENDOR)/etc/display/video_alignments.json
