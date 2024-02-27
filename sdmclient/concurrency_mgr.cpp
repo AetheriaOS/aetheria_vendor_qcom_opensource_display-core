14420c350b4ac252684a76823c5950416b0046fa
Sean Miller <quic_seanmill@quicinc.com>
2024-02-27 15:53:33 -0800

    SDM: add trace markers for setpowermode
    Change-Id: I119ce1628f9bf4d7b181a43c41733060fd8ca041

--- sdmclient/concurrency_mgr.cpp
+++ sdmclient/concurrency_mgr.cpp
@@ -981,6 +981,9 @@ DisplayError ConcurrencyMgr::SetPowerMode(uint64_t display, int32_t int_mode) {
     return kErrorParameters;
   }
 
+  DTRACE_BEGIN(
+      ("Setting power mode " + to_string(int_mode) + " on display " + to_string(display)).c_str());
+
   auto mode = static_cast<SDMPowerMode>(int_mode);
   bool is_builtin = false;
   bool is_power_off = false;
@@ -995,6 +998,7 @@ DisplayError ConcurrencyMgr::SetPowerMode(uint64_t display, int32_t int_mode) {
 
   if (mode == SDMPowerMode::POWER_MODE_ON &&
       !disp_->IsHWDisplayConnected(display)) {
+    DTRACE_END();
     return kErrorParameters;
   }
 
@@ -1016,12 +1020,14 @@ DisplayError ConcurrencyMgr::SetPowerMode(uint64_t display, int32_t int_mode) {
       SCOPE_LOCK(locker_[display]);
       if (sdm_display_[display]) {
         sdm_display_[display]->SetPendingPowerMode(mode);
+        DTRACE_END();
         return kErrorNone;
       }
     }
   }
   if (pending_power_mode_[display]) {
     DLOGW("Set power mode is not allowed during secure display session");
+    DTRACE_END();
     return kErrorNotSupported;
   }
 
@@ -1032,23 +1038,27 @@ DisplayError ConcurrencyMgr::SetPowerMode(uint64_t display, int32_t int_mode) {
     if (is_builtin) {
       DLOGE("Failed to get doze support Error = %d", status);
     }
+    DTRACE_END();
     return status;
   }
 
   if (!support && (mode == SDMPowerMode::POWER_MODE_DOZE ||
                    mode == SDMPowerMode::POWER_MODE_DOZE_SUSPEND)) {
+    DTRACE_END();
     return kErrorNotSupported;
   }
 
   SDMPowerMode last_power_mode = sdm_display_[display]->GetCurrentPowerMode();
 
   if (last_power_mode == mode) {
+    DTRACE_END();
     return kErrorNone;
   }
 
   auto error = CallDisplayFunction(display, &SDMDisplay::SetPowerMode, mode,
                                    false /* teardown */);
   if (error != kErrorNone) {
+    DTRACE_END();
     return error;
   }
   // Reset idle pc ref count on suspend, as we enable idle pc during suspend.
@@ -1063,6 +1073,7 @@ DisplayError ConcurrencyMgr::SetPowerMode(uint64_t display, int32_t int_mode) {
     pending_refresh_.set(UINT32(display));
   }
 
+  DTRACE_END();
   return kErrorNone;
 }
 
