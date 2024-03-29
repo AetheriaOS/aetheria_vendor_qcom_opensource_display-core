7b5f148916f3a848d66c5c61ad5bef91de1d9078
Avdhesh Singh Chauhan <quic_avdheshs@quicinc.com>
2024-03-29 13:06:24 +0530

    sdm: Add soc id for niobe 4k.
    Change-Id: I348cbdfb198090c52ae6a5db5994fa93ebae8cd0

--- sdm/include/utils/soc_info.h
+++ sdm/include/utils/soc_info.h
@@ -10,7 +10,8 @@
 #define kAnorakSocName "anorak"
 #define kSunSocId 618
 #define kSunSocName "sun"
-#define kNiobeSocId 629
+#define kNiobeSocId1 629
+#define kNiobeSocId2 652
 #define kNiobeSocName "niobe"
 
 #endif  // __SOC_INFO_H__
