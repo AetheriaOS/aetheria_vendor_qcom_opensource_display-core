yoriscape/display

display: sdm: Update YUV2RGB selection policy for Dolby unity CSC
Dolby vision profile 5 content currently requires a unity YUV2RGB csc matrix.
The existing  policy chooses the Dolby unity CSC based on extended content metadata
presence plus a layer color space of SMPTE170M-sRGB. This can result in misdetections
if the P5 layer expectations change or other sources use the extended content metadata
buffer.

Update the Dolby unity CSC selection policy to check the extended content metadata
header and drop color space expectations. Dolby unity CSC is now selected if the extended
content metadata identifies as Dolby P5 metadata.

Change-Id: I97f1df64a78b27379739de0f2efd9fb79fc21ddf

sdm/libs/dal/hw_device_drm.cpp
@@ -2149,13 +2149,20 @@ void HWDeviceDRM::SelectCscType(const LayerBuffer &input_buffer, DRMCscType *typ
   }
 
   // Override YUV to RGB CSC in DV P5 cases. If extended content metadata is used
-  // for other metadata types we will run into issues.
+  // and is DV's sync word, it should be DV. Then using full range to judge it's P5.
   bool extended_md_present = input_buffer.extended_content_metadata != nullptr &&
                               input_buffer.extended_content_metadata->size;
-  if (extended_md_present && (input_buffer.color_metadata.transfer == Transfer_SMPTE_170M
-                          || input_buffer.color_metadata.transfer == Transfer_sRGB)) {
-      *type = DRMCscType::kCscYuv2RgbDolbyVisionP5;
-      return;
+  if (extended_md_present) {
+    uint8_t *md = input_buffer.extended_content_metadata->metadataPayload;
+    if (md[0] == 0x0 && md[1] == 0x0 && md[2] == 0x0 && md[3] == 0x1 && md[4] == 0x7c &&
+        md[5] == 0x1) {
+      if ((input_buffer.color_metadata.range == Range_Full) ||
+          (input_buffer.color_metadata.transfer == Transfer_SMPTE_170M) ||
+          (input_buffer.color_metadata.transfer == Transfer_sRGB)) {
+        *type = DRMCscType::kCscYuv2RgbDolbyVisionP5;
+        return;
+      }
+    }
   }
 
   switch (input_buffer.color_metadata.colorPrimaries) {

AetheriaOS/aetheria_vendor_qcom_opensource_display-core

sdm/libs/dal/hw_device_drm.cpp
@@ -2440,13 +2440,20 @@ void HWDeviceDRM::SelectCscType(const LayerBuffer &input_buffer, DRMCscType *typ
   }
 
   // Override YUV to RGB CSC in DV P5 cases. If extended content metadata is used
-  // for other metadata types we will run into issues.
+  // and is DV's sync word, it should be DV. Then using full range to judge it's P5.
   bool extended_md_present = input_buffer.extended_content_metadata != nullptr &&
                               input_buffer.extended_content_metadata->size;
-  if (extended_md_present && (input_buffer.dataspace.transfer == QtiTransfer_SMPTE_170M
-                          || input_buffer.dataspace.transfer == QtiTransfer_sRGB)) {
-      *type = DRMCscType::kCscYuv2RgbDolbyVisionP5;
-      return;
+  if (extended_md_present) {
+    uint8_t *md = input_buffer.extended_content_metadata->metadataPayload;
+    if (md[0] == 0x0 && md[1] == 0x0 && md[2] == 0x0 && md[3] == 0x1 && md[4] == 0x7c &&
+        md[5] == 0x1) {
+      if ((input_buffer.dataspace.range == QtiRange_Full) ||
+          (input_buffer.dataspace.transfer == QtiTransfer_SMPTE_170M) ||
+          (input_buffer.dataspace.transfer == QtiTransfer_sRGB)) {
+        *type = DRMCscType::kCscYuv2RgbDolbyVisionP5;
+        return;
+      }
+    }
   }
 
   switch (input_buffer.dataspace.colorPrimaries) {