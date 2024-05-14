47ccfc7e7df2ab30d51bde04ef3ad5be324b9e28
Yuchao Ma <quic_yuchaom@quicinc.com>
2024-05-14 18:20:20 +0800

    sdm: Add new type of vnd service command to support trigger demura plugin
    This change adds new type of vnd service command to support trigger
    demura plugin.
    - adb shell "vndservice call display.qservice 64 i32 0 i32 5"
    
    Also adds a struct for demura backlight information in order to reuse
    parameter "kDemuraFeatureParamBacklightEvent" in SDM and demura.
    
    Change-Id: Id4662ebb8ff769eaccf7d828fd4bbfeca9f38b96

--- sdm/include/core/display_interface.h
+++ sdm/include/core/display_interface.h
@@ -369,6 +369,8 @@ enum PanelFeatureVendorServiceType {
   kTypeDemuraTnUserCtrl = 2,
   kTypeDeleteDemuraConfig = 3,
   kTypeDeleteDemuraTnConfig = 4,
+  /* Setter: None */
+  kTypeTriggerDemuraOemPlugIn = 5,
   PanelFeatureVendorServiceTypeMax,
 };
 
