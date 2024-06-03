34b375199f7966950f5b5b733d9c02db444d2d15
Avdhesh Singh Chauhan <quic_avdheshs@quicinc.com>
2024-06-03 14:34:40 +0530

    sdm: Query anamorphic metadata for each layer in layerBuffer.
    Change-Id: Ief38c138e2e07cd6df929467ade031108fc58a30

--- sdm/include/private/hw_info_types.h
+++ sdm/include/private/hw_info_types.h
@@ -771,6 +771,13 @@ struct HWScaleData {
   uint32_t cac_le_dst_h_offset = 0;
   uint32_t cac_le_dst_v_offset = 0;
   uint32_t cac_re_dst_v_offset = 0;
+
+  // Foveation params
+  uint32_t fov_mode = 0;
+  uint32_t cac_asym_phase_step_h = 0;
+  uint32_t cac_asym_phase_step_v = 0;
+  uint32_t cac_re_phase_step_v = 0;
+  uint32_t cac_re_asym_phase_step_v = 0;
 };
 
 struct HWDestScaleInfo {
