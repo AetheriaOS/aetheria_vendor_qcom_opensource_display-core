def7c867ce01b15987520f31ce593176eea7104f
BOREDDY MAHIDHAR <quic_bmahidha@quicinc.com>
2024-06-27 15:11:23 +0530

    sdm: Don't trigger refresh until register callback is not done
    Change-Id: I9c940f3413c6e6ddf74d71cccd28ebbd46f7a9e6

--- sdmclient/sdm_display_builder.cpp
+++ sdmclient/sdm_display_builder.cpp
@@ -643,7 +643,7 @@ int SDMDisplayBuilder::HandlePluggableDisplays(bool delay_hotplug) {
   }
 
   pending_hotplug_event_ = kHotPlugNone;
-  if (active_builtin_disp_id < kNumDisplays) {
+  if (active_builtin_disp_id < kNumDisplays && delay_hotplug) {
     cb_->WaitForCommitDone(active_builtin_disp_id, kClientTrustedUI);
   }
 
