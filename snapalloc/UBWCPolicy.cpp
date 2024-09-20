CodeLinaro-mirror/la_platform_vendor_opensource_display-core

snapalloc: Support for explicit ubwc formats
Identify explicit UBWC format using the modifier
"PIXEL_FORMAT_MODIFIER_EXPLICIT_UBWC" and force
UBWC allocation in snapalloc side irrespective of
usage values.

Support for Explicit UBWC formats was deprecated
as per design.During some valid UBWC camera usecases,
ImageReader is explicitly setting CPU bits. In snapalloc,
CPU bits take precedence over UBWC bits leading to linear
allocation for valid UBWC allocation cases. This change is
a workaround to handle above mentioned cases.

Change-Id: I68b51b9e07342f8e39ea12a82759a9335ed41c16

snapalloc/UBWCPolicy.cpp
@@ -39,6 +39,14 @@ void UBWCPolicy::Init(
 }
 
 bool UBWCPolicy::IsUBWCAlloc(BufferDescriptor desc) {
+  // Explicit UBWC formats passed by the clients.Ignore the usage bits and allow UBWC.
+  if (GetPixelFormatModifier(desc) ==
+      static_cast<uint64_t>(vendor_qti_hardware_display_common_PixelFormatModifier::
+                                PIXEL_FORMAT_MODIFIER_EXPLICIT_UBWC)) {
+    DLOGI("%s - Explicit ubwc format %d passed by the clients", __FUNCTION__, desc.format);
+    return true;
+  }
+
   // TODO: remove explicit R8 handling
   if (desc.format == vendor_qti_hardware_display_common_PixelFormat::R_8) {
     return false;

AetheriaOS/aetheria_vendor_qcom_opensource_display-core

snapalloc/UBWCPolicy.cpp
@@ -43,6 +43,14 @@ void UBWCPolicy::Init(
 }
 
 bool UBWCPolicy::IsUBWCAlloc(BufferDescriptor desc) {
+  // Explicit UBWC formats passed by the clients.Ignore the usage bits and allow UBWC.
+  if (GetPixelFormatModifier(desc) ==
+      static_cast<uint64_t>(vendor_qti_hardware_display_common_PixelFormatModifier::
+                                PIXEL_FORMAT_MODIFIER_EXPLICIT_UBWC)) {
+    ALOGI("%s - Explicit ubwc format %d passed by the clients", __FUNCTION__, desc.format);
+    return true;
+  }
+
   // TODO: remove explicit R8 handling
   if (desc.format == vendor_qti_hardware_display_common_PixelFormat::R_8) {
     return false;