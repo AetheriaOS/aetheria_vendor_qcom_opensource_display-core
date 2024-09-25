17ea8c023f83dbd238ceff8694d8a4e897157058
Tharaga Balachandran <quic_tbalacha@quicinc.com>
2024-09-25 17:16:31 -0400

    snapalloc: Skip graphics metadata blob init for YV12
    Graphics metadata was not previously initialized
    for YUV buffers. This change skips graphics
    metadata blob initialization for YV12
    until plane calculation delta is addressed.
    
    Change-Id: I05ceb93556f9fffe5b1dbab7c3a01d2962f931c8
    (cherry picked from commit 9ed9d081827abb00460c6c9cd1bd0e5f3bb53d8f)

--- snapalloc/SnapMetadataManager.cpp
+++ snapalloc/SnapMetadataManager.cpp
@@ -1106,7 +1106,8 @@ Error SnapMetadataManager::InitializeMetadata(
         DLOGE("Error initializing graphics metadata - ret val %d", ret);
       }
     } else {
-      DLOGE("Failed to get graphics metadata - retval %d", ret);
+      DLOGW("Failed to get graphics metadata - ret val %d, format %d, usage %d", ret,
+            out_desc.format, out_desc.usage);
     }
   } else {
     DLOGD_IF(enable_logs,
