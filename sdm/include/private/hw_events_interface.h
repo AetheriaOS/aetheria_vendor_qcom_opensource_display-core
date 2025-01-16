CodeLinaro-mirror/la_platform_hardware_qcom_display

sdm: Register for Panel Dead event for all the cores.
-Added support for multi instance of eventdrm base on core.
-Register the panel dead for core1.

Change-Id: I9c82c0c5742b1027dba779e69d34fedd8d33ef79

sdm/libs/core/hw_events_interface.h
@@ -24,7 +24,7 @@
 
 /*
 * Changes from Qualcomm Innovation Center are provided under the following license:
-* Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+* Copyright (c) 2022, 2024 Qualcomm Innovation Center, Inc. All rights reserved.
   SPDX-License-Identifier: BSD-3-Clause-Clear
 */
 
@@ -61,16 +61,17 @@ enum HWEvent {
 
 class HWEventsInterface {
  public:
-  virtual DisplayError Init(DisplayId display_id, DisplayType display_type,
+  virtual DisplayError Init(DisplayId display_id, uint32_t core_id, DisplayType display_type,
                             HWEventHandler *event_handler, const std::vector<HWEvent> &event_list,
                             const DPUCoreMux *hw_intf) = 0;
   virtual DisplayError Deinit() = 0;
   virtual DisplayError SetEventState(HWEvent event, bool enable, void *aux = nullptr) = 0;
 
   static DisplayError Create(DisplayId display_id, DisplayType display_type,
-                             HWEventHandler *event_handler, const std::vector<HWEvent> &event_list,
-                             const DPUCoreMux *hw_intf, HWEventsInterface **intf);
-  static DisplayError Destroy(HWEventsInterface *intf);
+                             HWEventHandler *event_handler,
+                             const std::map<uint32_t, std::vector<HWEvent>> &event_list,
+                             const DPUCoreMux *hw_intf, std::vector<HWEventsInterface *> *intf);
+  static DisplayError Destroy(std::vector<HWEventsInterface *> *intf);
 
  protected:
   virtual ~HWEventsInterface() { }

sdm/libs/core/drm/hw_events_drm.cpp
@@ -29,7 +29,7 @@
 
 /*
 * Changes from Qualcomm Innovation Center are provided under the following license:
-* Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+* Copyright (c) 2022, 2024 Qualcomm Innovation Center, Inc. All rights reserved.
   SPDX-License-Identifier: BSD-3-Clause-Clear
 */
 
@@ -84,21 +84,13 @@ namespace sdm {
 
 using drm_utils::DRMMaster;
 
-void HWEventsDRM::HandleDRMOpen(int& fd) {
-  for (int i = 0; i < core_id_map_.size(); i++) {
-    if (!core_id_map_[i]) {
-      continue;
-    }
-
-    if (i == 0) {
-      fd = drmOpen("msm_drm", nullptr);
-      return;
-    }
-
-    snprintf(path_, sizeof(path_), "/dev/dri/card%d", i);
-    fd = open(path_, O_RDWR | O_CLOEXEC, 0);
-    break;
+void HWEventsDRM::HandleDRMOpen(int &fd) {
+  if (core_id_ == 0) {
+    fd = drmOpen("msm_drm", nullptr);
+    return;
   }
+  snprintf(path_, sizeof(path_), "/dev/dri/card%d", core_id_);
+  fd = Sys::open_(path_, O_RDWR | O_CLOEXEC, 0);
 }
 
 DisplayError HWEventsDRM::InitializePollFd() {
@@ -282,21 +274,21 @@ void HWEventsDRM::PopulateHWEventData(const vector<HWEvent> &event_list) {
   InitializePollFd();
 }
 
-DisplayError HWEventsDRM::Init(DisplayId display_id, DisplayType display_type,
+DisplayError HWEventsDRM::Init(DisplayId display_id, uint32_t core_id, DisplayType display_type,
                                HWEventHandler *event_handler, const vector<HWEvent> &event_list,
                                const DPUCoreMux *hw_intf) {
   if (!event_handler)
     return kErrorParameters;
 
-  core_id_map_ = display_id.GetCoreIdMap();
-  static_cast<const DPUCoreMux *>(hw_intf)->GetDRMDisplayToken(&token_);
+  core_id_ = core_id;
+  static_cast<const DPUCoreMux *>(hw_intf)->GetDRMDisplayToken(core_id, &token_);
   is_primary_ = static_cast<const DPUCoreMux *>(hw_intf)->IsPrimaryDisplay();
   std::string backlight_path;
   static_cast<const DPUCoreMux *>(hw_intf)->GetPanelBrightnessBasePath(&backlight_path);
   brightness_node_ = backlight_path + "brightness";
 
-  DLOGI("Setup event handler for display %d-%d, CRTC %d, Connector %d", display_id.GetDisplayId(),
-        display_type, token_.crtc_id, token_.conn_id);
+  DLOGI("Setup event handler for display %d-%d, CRTC %d, Connector %d core_id %d",
+        display_id.GetDisplayId(), display_type, token_.crtc_id, token_.conn_id, core_id);
 
   event_handler_ = event_handler;
   poll_fds_.resize(event_list.size());
@@ -611,7 +603,7 @@ DisplayError HWEventsDRM::RegisterPanelDead(bool enable) {
   }
 
   if (ret) {
-    DLOGE("register panel dead enable:%d failed", enable);
+    DLOGE("register panel dead enable:%d failed conn_id %d", enable, token_.conn_id);
     return kErrorResources;
   }
 

sdm/libs/core/drm/hw_events_drm.h
@@ -29,7 +29,7 @@
 
 /*
 * Changes from Qualcomm Innovation Center are provided under the following license:
-* Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+* Copyright (c) 2022, 2024 Qualcomm Innovation Center, Inc. All rights reserved.
   SPDX-License-Identifier: BSD-3-Clause-Clear
 */
 
@@ -57,7 +57,7 @@ using std::vector;
 
 class HWEventsDRM : public HWEventsInterface {
  public:
-  virtual DisplayError Init(DisplayId display_id, DisplayType display_type,
+  virtual DisplayError Init(DisplayId display_id, uint32_t core_id, DisplayType display_type,
                             HWEventHandler *event_handler, const vector<HWEvent> &event_list,
                             const DPUCoreMux *hw_intf);
   virtual DisplayError Deinit();
@@ -135,7 +135,7 @@ class HWEventsDRM : public HWEventsInterface {
   uint32_t mmrm_index_ = UINT32_MAX;
   uint32_t power_event_index_ = UINT32_MAX;
   uint32_t vm_release_event_index_ = UINT32_MAX;
-  std::bitset<8> core_id_map_ = 0;
+  uint32_t core_id_ = 0;
   char path_[64];
 };
 

AetheriaOS/aetheria_vendor_qcom_opensource_display-core

sdm/include/private/hw_events_interface.h
@@ -24,7 +24,7 @@
 
 /*
 * Changes from Qualcomm Innovation Center are provided under the following license:
-* Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
+* Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
   SPDX-License-Identifier: BSD-3-Clause-Clear
 */
 
@@ -61,16 +61,17 @@ enum HWEvent {
 
 class HWEventsInterface {
  public:
-  virtual DisplayError Init(DisplayId display_id, SDMDisplayType display_type,
+  virtual DisplayError Init(DisplayId display_id, uint32_t core_id, SDMDisplayType display_type,
                             HWEventHandler *event_handler,
                             const std::vector<HWEvent> &event_list) = 0;
   virtual DisplayError Deinit() = 0;
   virtual DisplayError SetEventState(HWEvent event, bool enable, void *aux = nullptr) = 0;
 
   static DisplayError Create(DisplayId display_id, SDMDisplayType display_type,
-                             HWEventHandler *event_handler, const std::vector<HWEvent> &event_list,
-                             HWEventsInterface **intf);
-  static DisplayError Destroy(HWEventsInterface *intf);
+                             HWEventHandler *event_handler,
+                             const std::map<uint32_t, std::vector<HWEvent>> &event_list,
+                             std::vector<HWEventsInterface *> *intf);
+  static DisplayError Destroy(std::vector<HWEventsInterface *> *intf);
 
  protected:
   virtual ~HWEventsInterface() { }

sdm/libs/dal/hw_events_drm.cpp
@@ -29,7 +29,7 @@
 
 /*
 * Changes from Qualcomm Innovation Center are provided under the following license:
-* Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
+* Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
   SPDX-License-Identifier: BSD-3-Clause-Clear
 */
 
@@ -84,21 +84,13 @@ namespace sdm {
 
 using drm_utils::DRMMaster;
 
-void HWEventsDRM::HandleDRMOpen(int& fd) {
-  for (int i = 0; i < core_id_map_.size(); i++) {
-    if (!core_id_map_[i]) {
-      continue;
-    }
-
-    if (i == 0) {
-      fd = drmOpen("msm_drm", nullptr);
-      return;
-    }
-
-    snprintf(path_, sizeof(path_), "/dev/dri/card%d", i);
-    fd = open(path_, O_RDWR | O_CLOEXEC, 0);
-    break;
+void HWEventsDRM::HandleDRMOpen(int &fd) {
+  if (core_id_ == 0) {
+    fd = drmOpen("msm_drm", nullptr);
+    return;
   }
+  snprintf(path_, sizeof(path_), "/dev/dri/card%d", core_id_);
+  fd = Sys::open_(path_, O_RDWR | O_CLOEXEC, 0);
 }
 
 DisplayError HWEventsDRM::InitializePollFd() {
@@ -279,20 +271,20 @@ void HWEventsDRM::PopulateHWEventData(const vector<HWEvent> &event_list) {
   InitializePollFd();
 }
 
-DisplayError HWEventsDRM::Init(DisplayId display_id, SDMDisplayType display_type,
+DisplayError HWEventsDRM::Init(DisplayId display_id, uint32_t core_id, SDMDisplayType display_type,
                                HWEventHandler *event_handler, const vector<HWEvent> &event_list) {
   if (!event_handler)
     return kErrorParameters;
 
-  core_id_map_ = display_id.GetCoreIdMap();
-  event_handler->GetDRMDisplayToken(&token_);
+  core_id_ = core_id;
+  event_handler->GetDRMDisplayToken(core_id_, &token_);
   is_primary_ = event_handler->IsPrimaryDisplay();
   std::string backlight_path;
   event_handler->GetPanelBrightnessBasePath(&backlight_path);
   brightness_node_ = backlight_path + "brightness";
 
-  DLOGI("Setup event handler for display %d-%d, CRTC %d, Connector %d", display_id.GetDisplayId(),
-        display_type, token_.crtc_id, token_.conn_id);
+  DLOGI("Setup event handler for display %d-%d, CRTC %d, Connector %d core_id %d",
+        display_id.GetDisplayId(), display_type, token_.crtc_id, token_.conn_id, core_id);
 
   event_handler_ = event_handler;
   poll_fds_.resize(event_list.size());
@@ -612,7 +604,7 @@ DisplayError HWEventsDRM::RegisterPanelDead(bool enable) {
   }
 
   if (ret) {
-    DLOGE("register panel dead enable:%d failed", enable);
+    DLOGE("register panel dead enable:%d failed conn_id %d", enable, token_.conn_id);
     return kErrorResources;
   }
 

sdm/libs/dal/hw_events_drm.h
@@ -29,7 +29,7 @@
 
 /*
 * Changes from Qualcomm Innovation Center are provided under the following license:
-* Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
+* Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
   SPDX-License-Identifier: BSD-3-Clause-Clear
 */
 
@@ -57,7 +57,7 @@ using std::vector;
 
 class HWEventsDRM : public HWEventsInterface {
  public:
-  virtual DisplayError Init(DisplayId display_id, SDMDisplayType display_type,
+  virtual DisplayError Init(DisplayId display_id, uint32_t core_id, SDMDisplayType display_type,
                             HWEventHandler *event_handler, const vector<HWEvent> &event_list);
   virtual DisplayError Deinit();
   virtual DisplayError SetEventState(HWEvent event, bool enable, void *aux = nullptr);
@@ -134,7 +134,7 @@ class HWEventsDRM : public HWEventsInterface {
   uint32_t power_event_index_ = UINT32_MAX;
   uint32_t vm_release_event_index_ = UINT32_MAX;
   std::bitset<HW_EVENT_MAX> registered_hw_events_ = {};
-  std::bitset<8> core_id_map_ = 0;
+  uint32_t core_id_ = 0;
   char path_[64];
 };
 