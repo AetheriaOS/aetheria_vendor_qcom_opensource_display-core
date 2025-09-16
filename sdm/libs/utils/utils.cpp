d8aa3ac9cb571126e435be5bfaad3b126fea790e
Shreyas Narayan <shrnar@qti.qualcomm.com>
2025-09-16 02:28:49 -0700

    Revert "display-core: add macros for kera target"
    This reverts commit 82774d9978989efe946f0e2c05fd943e60eb106a.
    
    Reason for revert: <Change will be brought in via mainline re-target>
    
    Change-Id: Ibd2fad4ec3c37728fe6a35d11d29765cda60f290

--- sdm/libs/utils/utils.cpp
+++ sdm/libs/utils/utils.cpp
@@ -218,8 +218,6 @@ const char* GetSocName() {
     case kTunaSocId3:
     case kKeraSocId1:
     case kKeraSocId2:
-    case kKeraSocId3:
-    case kKeraSocId4:
       return kSunSocName;
     case kNiobeSocId1:
     case kNiobeSocId2:
